2ded4ab928d39b354359150a0706920f67e615a2
Victoria Johnson <2585743J@student.gla.ac.uk>
2021-11-23 05:44:12 +0000

    Adjustments

--- review_app/review_app/urls.py
+++ review_app/review_app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('rally/', include('rally.urls')),
     path('admin/', admin.site.urls),
+    path('', include('rally.urls')),
 ]
